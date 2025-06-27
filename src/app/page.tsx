408c94e1e09f9fbcb0ead9d293aebd48c51174ae
Brahim Boussada <brahimboussada@protonmail.com>
2025-06-27 23:41:10 +0300

    log: 2025-06-27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,27 @@ export default function Home() {
     <Container>
       <NoteWrapper>
         <Note>
-          <N>1.</N> <Key>Geoffrey Hinton</Key> played a major role in the
-          foundation of modern AI.
+          <N>1. </N>
+          <Key>Geoffrey Hinton</Key> played a major role in the foundation of
+          modern AI.
         </Note>
 
         <Note>
-          <N>2.</N> The World Wide Web, HTML, URLs, and HTTP were invented by{" "}
+          <N>2. </N>The World Wide Web, HTML, URLs, and HTTP were invented by{" "}
           <Key>Tim Berners-Lee</Key> at CERN beginning in 1989.
         </Note>
 
         <Note>
-          <N>3.</N> <Key>Atoms</Key> are composed of a nucleus with protons and
-          neutrons, surrounded by electrons.
+          <N>3. </N>
+          <Key>Atoms</Key> are composed of a nucleus with protons and neutrons,
+          surrounded by electrons.
+        </Note>
+
+        <Note>
+          <N>4. </N>
+          <Key>Cybernetics</Key> is the science of how systems, both natural and
+          artificial, self-regulate by measuring their output and making
+          adjustments to achieve a goal.
         </Note>
       </NoteWrapper>
     </Container>
