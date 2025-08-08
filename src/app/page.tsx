577ed0ae34ede20b624588d313844215b91ce8fc
Brahim Boussada <brahimboussada@protonmail.com>
2025-08-08 23:56:43 +0300

    log: 08-08-2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,120 +11,101 @@ export default function Home() {
           <Key>Geoffrey Hinton</Key> played a major role in the foundation of
           modern AI.
         </Note>
-
         <Note>
           <N>2. </N>The World Wide Web, HTML, URLs, and HTTP were invented by{" "}
           <Key>Tim Berners-Lee</Key> at CERN beginning in 1989.
         </Note>
-
         <Note>
           <N>3. </N>
           <Key>Atoms</Key> are composed of a nucleus with protons and neutrons,
           surrounded by electrons.
         </Note>
-
         <Note>
           <N>4. </N>
           <Key>Cybernetics</Key> is the science of how systems, both natural and
           artificial, self-regulate by measuring their output and making
           adjustments to achieve a goal.
         </Note>
-
         <Note>
           <N>5. </N>
           <Key>The electromagnetic spectrum</Key> is the full range of all
           electromagnetic wave frequencies, including radio waves, microwaves,
           infrared, visible light, ultraviolet, X-rays, and gamma rays.
         </Note>
-
         <Note>
           <N>6. </N>A <Key>quantum</Key> is the smallest measurable unit of
           energy or matter that behaves according to the rules of quantum
           mechanics.
         </Note>
-
         <Note>
           <N>7. </N>
           <Key>Roy Fielding</Key> created the REST concept in his 2000 PhD
           thesis to define a scalable, stateless architecture for web APIs.
         </Note>
-
         <Note>
           <N>8. </N>
           <Key>Linux</Key> was developed in 1991 by Linus Torvalds as a free and
           open-source operating system kernel.
         </Note>
-
         <Note>
           <N>9. </N>A <Key>lexicon</Key> is a specialized dictionary that
           systematically records the words of a language, including their
           meanings, origins, and usage.
         </Note>
-
         <Note>
           <N>9. </N>
           <Key>Synthesis</Key> is the process of producing a new substance,
           idea, or system by chemically, logically, or creatively combining two
           or more distinct components or elements.
         </Note>
-
         <Note>
           <N>10. </N>
           <Key>Methodology</Key> is the systematic set of methods, rules, and
           principles used to conduct research or carry out a particular process.
         </Note>
-
         <Note>
           <N>11. </N>
           An <Key>algorithm</Key> is a defined sequence of logical steps or
           rules designed to perform a specific task or solve a particular
           problem efficiently.
         </Note>
-
         <Note>
           <N>12. </N>A <Key>prodigy</Key> is a young person who demonstrates
           exceptional talent or ability in a particular field at an unusually
           early age.
         </Note>
-
         <Note>
           <N>13. </N>
           <Key>Etymology</Key> is the study of the origin and history of words
           and how their meanings and forms have evolved over time.
         </Note>
-
         <Note>
           <N>14. </N>
           <Key>Numbers</Key> are mathematical symbols used to count, measure,
           label, and represent quantities or values.
         </Note>
-
         <Note>
           <N>15. </N>
           <Key>DNS</Key> is a system that translates human-readable domain names
           into numerical IP addresses, allowing computers to locate and connect
           to each other on the internet.
         </Note>
-
         <Note>
           <N>16. </N>
           <Key>Cognition</Key> refers to the mental action or process of
           acquiring knowledge and understanding through thought, experience, and
           the senses.
         </Note>
-
         <Note>
           <N>17. </N>
           <Key>Ephemera</Key> means things that are temporary or short-lived,
           such as moments, events, or objects that don’t last long.
         </Note>
-
         <Note>
           <N>18. </N>
           <Key>Relativity</Key> is the theory that space and time change
           depending on speed and gravity.
         </Note>
-
         <Note>
           <N>19. </N>
           <Key>Intelligence</Key> is The ability to acquire and apply knowledge
@@ -132,25 +113,31 @@ export default function Home() {
           complex concepts, as well as evaluating moral and ethical
           considerations.
         </Note>
-
         <Note>
           <N>20. </N>
           <Key>Topology</Key> is the arrangement and connection of parts within
           a system, focusing on how elements relate rather than their exact
           form.
         </Note>
-
         <Note>
           <N>21. </N>
           <Key>G-force</Key> is the measure of acceleration acting on an object,
           expressed as a multiple of Earth's gravity (1 G = 9.8 m/s²).
         </Note>
-
         <Note>
           <N>22. </N>
           <Key>Geology</Key> is the scientific study of Earth's physical
           structure, composition, and the dynamic processes such as tectonic
           activity, volcanism, and erosion that have shaped it over geological
+          time.
+        </Note>
+
+        <Note>
+          <N>23. </N>
+          <Key>Copper</Key> is a reddish-brown metal prized for its intrinsic
+          value, conductivity, and durability. Over time, it naturally develops
+          a protective green patina that characterizes its surface and inhibits
+          further corrosion.
         </Note>
       </NoteWrapper>
     </Container>
